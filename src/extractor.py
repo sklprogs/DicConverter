e29f0a8a3f1ba63d785a6417118e9b047d6f4ccb
skl.progs <skl.progs@gmail.com>
2020-05-04 02:30:39 +0300

    extractor.Extractor

--- src/extractor.py
+++ src/extractor.py
@@ -6,17 +6,78 @@ from skl_shared.localize import _
 import get as gt
 
 
+class Extractor:
+    
+    def __init__(self):
+        self.set_values()
+    
+    def set_values(self):
+        self.Success = True
+        self.simple = []
+        self.phrases = []
+        self.subjects = []
+    
+    def debug(self):
+        f = '[MTExtractor] extractor.Extractor.debug'
+        if self.Success:
+            headers = ['SIMPLE','PHRASES','SUBJECT']
+            iterable = [self.simple,self.phrases,self.subjects]
+            mes = sh.FastTable (iterable = iterable
+                               ,headers  = headers
+                               ,maxrow   = 50
+                               ,maxrows  = 1000
+                               ).run()
+            sh.com.run_fast_debug(mes)
+        else:
+            sh.com.cancel(f)
+    
+    def set_phrases(self):
+        f = '[MTExtractor] extractor.Extractor.set_phrases'
+        if self.Success:
+            if self.iparse.chunks1:
+                for part1 in self.iparse.chunks1:
+                    if part1:
+                        chunks = part1.split(b'\x00')
+                        chunks = [chunk.decode(gt.CODING,'replace') \
+                                  for chunk in chunks if chunk
+                                 ]
+                        self.simple.append(chunks[0])
+                        if len(chunks) == 1:
+                            self.phrases.append(chunks[0])
+                        else:
+                            self.phrases.append(chunks[1])
+                    else:
+                        sh.com.rep_empty(f)
+            else:
+                sh.com.rep_empty(f)
+                self.Success = False
+        else:
+            sh.com.cancel(f)
+    
+    def parse_typein(self):
+        f = '[MTExtractor] extractor.Extractor.parse_typein'
+        if self.Success:
+            self.iparse = gt.Parser(gt.objs.get_files().iwalker.get_typein1())
+            self.iparse.parsel_loop(10)
+            self.subjects = list(self.iparse.xplain2)
+            mes = _('Number of chunks: {}').format(len(self.iparse.chunks1))
+            sh.objs.get_mes(f,mes,True).show_debug()
+            self.Success = self.iparse.Success
+        else:
+            sh.com.cancel(f)
+    
+    def run(self):
+        self.parse_typein()
+        self.set_phrases()
+
+
 if __name__ == '__main__':
-    f = '[MTExtractor] tests.__main__'
+    f = '[MTExtractor] extractor.__main__'
     gt.PATH = '/home/pete/.config/mclient/dics'
     gt.DEBUG = False
     timer = sh.Timer(f)
     timer.start()
-    #get_stems1
-    #get_typein1
-    iparse = gt.Parser(gt.objs.get_files().iwalker.get_typein2())
-    iparse.parsel_loop(1)
+    iextract = Extractor()
+    iextract.run()
     timer.end()
-    iparse.debug(75)
-    mes = _('Number of chunks: {}').format(len(iparse.chunks1))
-    sh.objs.get_mes(f,mes,True).show_debug()
+    iextract.debug()

32d57246c5e6f32b720ebcd724fa751ce72fa636
skl.progs <skl.progs@gmail.com>
2020-05-04 19:44:09 +0300

    extractor.Extractor.run_lang

--- src/extractor.py
+++ src/extractor.py
@@ -11,10 +11,14 @@ class Extractor:
     def __init__(self):
         self.set_values()
     
+    def reset(self):
+        self.set_values()
+    
     def set_values(self):
         self.Success = True
         self.simple = []
         self.phrases = []
+        self.iparse = None
     
     def debug(self):
         f = '[MTExtractor] extractor.Extractor.debug'
@@ -58,11 +62,6 @@ class Extractor:
         if self.Success:
             self.iparse = gt.Parser(file)
             self.iparse.parsel_loop(limit)
-            mes = _('Processed chunks: {}/{}')
-            mes = mes.format (self.iparse.origcnt
-                             ,len(self.iparse.chunks1)
-                             )
-            sh.objs.get_mes(f,mes,True).show_info()
             self.Success = self.iparse.Success
         else:
             sh.com.cancel(f)
@@ -80,30 +79,64 @@ class Extractor:
         else:
             sh.com.cancel(f)
     
-    def run(self):
-        #'acceleration measured in g'
-        file = gt.objs.get_files().iwalker.get_typein1()
-        self.parse_typein(file,10)
-        self.set_phrases()
-        #self.debug()
-        art_nos = []
-        for i in range(len(self.simple)):
-            art_no = self.translate(self.simple[i])
-            #art_nos += gt.com.parse3(art_no)
-            if art_no:
-                art_no = gt.com.parse3(art_no)
-                art_no = [item[0] for item in art_no if item]
-                art_nos.append(art_no)
+    def run_lang(self,lang=1,limit=0):
+        f = '[MTExtractor] extractor.Extractor.run_lang'
+        if self.Success:
+            if lang == 1:
+                file = gt.objs.get_files().iwalker.get_typein1()
             else:
-                art_nos.append(_('N/A'))
-        assert len(self.simple) == len(self.phrases)
-        headers = ('PHRASE','SUBJECT','ARTNO')
-        iterable = [self.phrases,self.iparse.xplain2,art_nos]
-        mes = sh.FastTable (iterable = iterable
-                           ,headers  = headers
-                           ,maxrow   = 60
-                           ).run()
-        sh.com.run_fast_debug(mes)
+                gt.LANG1, gt.LANG2 = gt.LANG2, gt.LANG1
+                gt.objs.get_files().reset()
+                file = gt.objs.files.iwalker.get_typein1()
+            timer = sh.Timer(f)
+            timer.start()
+            sh.STOP_MES = True
+            self.parse_typein(file,limit)
+            self.set_phrases()
+            #self.debug()
+            art_nos = []
+            for i in range(len(self.simple)):
+                art_no = self.translate(self.simple[i])
+                if art_no:
+                    art_no = gt.com.parse3(art_no)
+                    art_no = [item[0] for item in art_no if item]
+                    art_nos.append(art_no)
+                else:
+                    art_nos.append(_('N/A'))
+            sh.STOP_MES = False
+            timer.end()
+            assert len(self.simple) == len(self.phrases)
+            mes = _('Processed chunks (total/parsed/translated): {}/{}/{}')
+            translated = 0
+            for art_no in art_nos:
+                if art_no != _('N/A'):
+                    translated += 1
+            mes = mes.format (self.iparse.origcnt
+                             ,len(self.phrases)
+                             ,translated
+                             )
+            sh.objs.get_mes(f,mes,True).show_info()
+            headers = ('PHRASE','SUBJECT','ARTNO')
+            iterable = [self.phrases,self.iparse.xplain2,art_nos]
+            mes = sh.FastTable (iterable = iterable
+                               ,headers  = headers
+                               ).run()
+            #TODO: elaborate path
+            if lang == 1:
+                filew = '/home/pete/tmp/result_en.txt'
+            else:
+                filew = '/home/pete/tmp/result_ru.txt'
+            sh.WriteTextFile (file    = filew
+                             ,Rewrite = True
+                             ).write(mes)
+            sh.Launch(filew).launch_default()
+        else:
+            sh.com.cancel(f)
+    
+    def run(self):
+        self.run_lang(lang=1,limit=0)
+        self.reset()
+        self.run_lang(lang=2,limit=0)
         
 
 
@@ -111,12 +144,5 @@ if __name__ == '__main__':
     f = '[MTExtractor] extractor.__main__'
     gt.PATH = '/home/pete/.config/mclient/dics'
     gt.DEBUG = False
-    # Do this after changing the current language
     gt.objs.get_files().reset()
-    timer = sh.Timer(f)
-    timer.start()
-    iextract = Extractor()
-    iextract.run()
-    pattern = 'acceleration measured in g'
-    #pattern = 'absolute measurements'
-    timer.end()
+    Extractor().run()
